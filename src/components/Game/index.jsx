58c7852dcadb81a6eface5e8ad322e58eada07f8
publizm <publizm@gmail.com>
2020-01-16 12:32:08 +0900

    docs: complete tic-tac-toe with class Component

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -7,123 +7,104 @@ export class Game extends Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      isLastPlayer: false,
-      xPlayer: [],
-      oPlayer: [],
-      histories: [],
       count: 0,
-      flag: false,
+      histories: [
+        {
+          count: null,
+          index: null,
+          history: Array(9).fill(null),
+        },
+      ],
+      isLastPlayer: true,
     };
     Object.getOwnPropertyNames(Game.prototype).forEach(key => (this[key] = this[key].bind(this)));
   }
 
   addHistory(player, index) {
-    const { squares, oPlayer, xPlayer, count, histories, isLastPlayer, flag } = this.state;
-
+    const { squares, count, histories, isLastPlayer } = this.state;
     if (squares[index]) return;
 
-    // if (flag) {
-    //   console.log(histories);
-    //   const refreshHistory = histories.filter((history, idx) => (idx <= index ? history : false));
+    const newSquares = squares.slice();
+    newSquares[index] = isLastPlayer ? 'X' : 'O';
 
-    //   const { history: refreshSquares } = histories.filter((history, idx) =>
-    //     idx <= index ? history : false,
-    //   )[index];
+    const newCount = squares.slice(0, count + 1).length;
+    const prevCount = Math.max(...histories.map((history, index) => history.count)) + 1;
+    const newHistory =
+      prevCount > count
+        ? histories.filter((history, index) => index <= count)
+        : histories.filter((history, index) => history.count < count + 1);
 
-    //   this.setState({
-    //     squares: refreshSquares.map((square, idx) =>
-    //       idx === index ? 'X' : square === 'X' ? 'X' : square === 'O' ? 'O' : null,
-    //     ),
-    //     oPlayer: [...oPlayer, index],
-    //     count: count + 1,
-    //     histories: [...refreshHistory, { count: count, index, history: squares }],
-    //     isLastPlayer: !isLastPlayer,
-    //   });
-    // }
-
-    if (player) {
-      this.setState(
-        {
-          squares: squares.map((square, idx) =>
-            idx === index ? 'X' : square === 'X' ? 'X' : square === 'O' ? 'O' : null,
-          ),
-          oPlayer: [...oPlayer, index],
-          count: count + 1,
-          histories: [...histories, { count: count, index, history: squares }],
-          isLastPlayer: !isLastPlayer,
-        },
-        () => {
-          console.log(this.state);
-        },
-      );
-    } else {
-      this.setState(
-        {
-          squares: squares.map((square, idx) =>
-            idx === index ? 'O' : square === 'O' ? 'O' : square === 'X' ? 'X' : null,
-          ),
-          xPlayer: [...xPlayer, index],
-          count: count + 1,
-          histories: [...histories, { count: count, index, history: squares }],
-          isLastPlayer: !isLastPlayer,
-        },
-        () => {
-          console.log(this.state);
-        },
-      );
-    }
+    this.setState({
+      squares: newSquares,
+      count: newCount,
+      histories: [...newHistory, { count: count, index, history: newSquares }],
+      isLastPlayer: !isLastPlayer,
+    });
   }
 
   jumpTo(index) {
-    const { squares, oPlayer, xPlayer, count, histories, isLastPlayer } = this.state;
-    const refreshHistory = histories.filter((history, idx) => (idx <= index ? history : false));
+    const { histories, count } = this.state;
 
-    // const currentCount = Math.max(...histories.map(history => history.count)) + 1;
-
-    // console.log(currentCount, count);
     const { history: refreshSquares } = histories.filter((history, idx) =>
       idx <= index ? history : false,
     )[index];
 
-    this.setState(
-      {
-        squares: [...refreshSquares],
-        count: index,
-        flag: true,
-      },
-      () => {
-        console.log(this.state);
-      },
-    );
+    this.setState({
+      squares: [...refreshSquares],
+      count: index,
+      isLastPlayer: index % 2 === 0,
+    });
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return `Winner is ${squares[a]}`;
+      }
+    }
+    return `Next Player ${this.state.isLastPlayer ? 'X' : 'O'}`;
   }
 
   render() {
     const { squares, isLastPlayer, histories } = this.state;
+
+    const historyList = histories.map((history, index) => {
+      const guide = index ? 'Go to move #' + index : 'Go to game start';
+      return (
+        <li key={index}>
+          <button onClick={() => this.jumpTo(index)}>{guide}</button>
+        </li>
+      );
+    });
+
+    const squaresList = squares.map((square, index) => (
+      <Square
+        square={square}
+        key={index}
+        index={index}
+        className={index % 3 ? null : 'first'}
+        isLastPlayer={isLastPlayer}
+        addHistory={this.addHistory}
+      />
+    ));
+
     return (
       <div className="game">
-        <Board squares={squares}>
-          {squares.map((square, index) => (
-            <Square
-              square={square}
-              key={index}
-              index={index}
-              className={index % 3 ? null : 'first'}
-              isLastPlayer={isLastPlayer}
-              addHistory={this.addHistory}
-            />
-          ))}
-        </Board>
+        <Board squares={squares}>{squaresList}</Board>
         <div className="game-info">
-          <div className="status">Next Player {isLastPlayer ? 'X' : 'O'}</div>
-          <ol>
-            {histories.map((history, index) => (
-              <li key={index}>
-                <button onClick={() => this.jumpTo(index)}>
-                  Go to Game {history.count ? history.count : 'start'}
-                </button>
-              </li>
-            ))}
-          </ol>
+          <div className="status">{this.calculateWinner(squares)}</div>
+          <ol>{historyList}</ol>
         </div>
       </div>
     );
